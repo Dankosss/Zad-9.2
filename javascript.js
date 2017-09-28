c1a4dd2fe469f174ba22c839e59ced96933f2626
m <m>
2017-09-28 16:04:42 +0200

    javascript

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var femaleNames = ['Asia', 'Kasia', 'Ania', 'Ilona'];
+console.log(femaleNames);
+var maleNames = ['Michal', 'Tomek', 'Grzegorz', 'Jarek'];
+console.log(maleNames);
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+	 allNames.push(newName);
+     console.log(allNames);
+}
