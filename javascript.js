20e23d7659df5b67d18b6dfc11316dff406ee730
m <m>
2017-09-28 16:07:03 +0200

    fixed

--- javascript.js
+++ javascript.js
@@ -6,6 +6,6 @@ var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 var newName = 'Marian';
 if (allNames.indexOf(newName) === -1) {
-	 allNames.push(newName);
-     console.log(allNames);
+	allNames.push(newName);
+ 	console.log(allNames);
 }
